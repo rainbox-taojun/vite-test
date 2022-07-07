2aba4ee7089a40e825e951c5ad614d258f5132a8
taojun1994 <taojun1994@gmail.com>
2022-07-07 16:57:13 +0800

    feat: 任务贯通情况面板

--- src/views/home/components/MissionStatus.vue
+++ src/views/home/components/MissionStatus.vue
@@ -1,5 +1,11 @@
 <script setup>
 // 任务贯通情况
+const list = ref([
+  { label: '七张问题清单', link: '/' },
+  { label: '疫情在线', link: '/' },
+  { label: '防汛防台', link: '/' },
+  { label: '七张问题清单', link: '/' }
+])
 </script>
 
 <template>
@@ -10,5 +16,47 @@
     <template v-slot:title-right>
       <router-link to="/">更多></router-link>
     </template>
+
+    <ul class="list">
+      <li
+        v-for="item in list"
+        class="list-item"
+      >
+        {{ item.label }}
+      </li>
+    </ul>
   </PanelV1>
 </template>
+
+<style lang="scss" scoped>
+.mission-status {
+  .list {
+    list-style-type: none;
+    margin: 0;
+    padding: 10px 13px;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 100%;
+
+    .list-item {
+      margin-bottom: 14px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: calc(50% - 6.5px);
+      height: calc(50% - 7px);
+      font-size: 18px;
+      color: #FFFFFF;
+      line-height: 25px;
+      background-image: url(@/assets/mission-status-card_bg.png);
+      background-size: 100% 100%;
+      cursor: pointer;
+
+      &:nth-child(even) {
+        margin-left: 13px;
+      }
+    }
+  }
+}
+</style>
